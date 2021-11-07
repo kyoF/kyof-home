13e1a15079706c7a4d0162e9cbb5f55b5cc8e22e
ekusoyk <fujiki.kyosuke@gmail.com>
2021-11-07 23:50:55 +0900

    draftjsが機能しているかの確認とコマンドの操作を追加

--- src/components/RichTextEditor.tsx
+++ src/components/RichTextEditor.tsx
@@ -1,16 +1,29 @@
 import React from "react";
-import { Editor, EditorState } from "draft-js";
+import { Editor, EditorState, RichUtils, DraftEditorCommand } from "draft-js";
 import "draft-js/dist/Draft.css";
 
 function RichTextEditor() {
   const [editorState, setEditorState] = React.useState<EditorState>(
     EditorState.createEmpty()
   );
+  const handleKeyCommand = (command: DraftEditorCommand) => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      setEditorState(newState);
+      return "handled";
+    }
+    return "not-handled";
+  };
 
   return (
     <div>
       <p>ddd</p>
-      <Editor editorState={editorState} onChange={setEditorState} />
+      <Editor
+        editorState={editorState}
+        onChange={setEditorState}
+        placeholder="test"
+        handleKeyCommand={handleKeyCommand}
+      />
     </div>
   );
 }
