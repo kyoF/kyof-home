2a607a65c48a730044900ea11a0505f2b34f0f10
ekusoyk <fujiki.kyosuke@gmail.com>
2022-01-13 23:54:21 +0900

    reactrouterを使う準備

---
+++ src/features/page.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+
+const page = () => {
+  return (
+    <div>
+      <Link to="/">Blog</Link>
+    </div>
+  );
+};
+
+export default page;
